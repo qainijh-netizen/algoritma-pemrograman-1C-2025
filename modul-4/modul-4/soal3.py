4134e180bab3e1028eb922b7b811283292cc8c97
qurrotun aini <qainijh@gmail.com>
2025-12-01 22:35:56 +0700

    250441100073-QurrotunAini-AsprakKakAinunSofia

---
+++ modul-4/modul-4/soal3.py
@@ -0,0 +1,13 @@
+n = int(input("Masukkan angka n: "))
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end="")
+
+    for k in range((n - i) * 2):
+        print(" ", end="")
+
+    for j in range(i, 0, -1):
+        print(j, end="")
+
+    print()
